d1e163022a6f3e903f5065f6ad27e12be0d72d85
itsAboo <mew.thanachot@gmail.com>
2024-07-10 19:26:32 +0700

    fix my-booking

--- src/app/account/(auth)/my-booking/page.tsx
+++ src/app/account/(auth)/my-booking/page.tsx
@@ -1,19 +1,38 @@
-import { getAllMyBooking } from "@/api/auth";
 import BookingList from "@/components/booking/booking-list";
 import MyBookingSkeleton from "@/components/skeleton/my-booking.skeleton";
+import { lucia } from "@/lib/lucia";
+import { IBooking } from "@/models/booking.model";
+import { cookies } from "next/headers";
 import { Suspense } from "react";
 
 const MyBooking = async () => {
-  const bookings = await getAllMyBooking();
-  return (
-    <>
-      {bookings.length < 1 ? (
-        <h1 className="mt-5 text-center">No result</h1>
-      ) : (
-        <BookingList bookings={bookings} />
-      )}
-    </>
-  );
+  try {
+    const response = await fetch(`${process.env.AUTH_API}/booking`, {
+      headers: {
+        Authorization: `Bearer ${JSON.stringify(
+          cookies().get(lucia.sessionCookieName)
+        )}`,
+      },
+    });
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+    const data = await response.json();
+    const bookings = data.bookings as IBooking[];
+    return (
+      <>
+        {bookings.length < 1 ? (
+          <h1 className="mt-5 text-center">No result</h1>
+        ) : (
+          <BookingList bookings={bookings} />
+        )}
+      </>
+    );
+  } catch (error: any) {
+    console.log(error);
+    throw error;
+  }
 };
 
 export default function MyBookingPage() {
