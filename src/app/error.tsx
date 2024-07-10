843664770e22fd11da16f1fcf47a8bb9fd74ca2b
itsAboo <mew.thanachot@gmail.com>
2024-07-10 18:44:40 +0700

    fix error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,22 @@
 "use client";
 
-export default function ErrorPage() {
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+export default function ErrorPage({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
   return (
     <div className="flex justify-center items-center min-h-screen">
       <h1>Something went wrong</h1>
+      <Button onClick={() => reset()}>Try again</Button>
     </div>
   );
 }
