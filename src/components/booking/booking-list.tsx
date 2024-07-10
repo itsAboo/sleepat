b99810ddc8cbc51908aeb74fd0068bc006187b7c
itsAboo <mew.thanachot@gmail.com>
2024-07-10 18:39:29 +0700

    fix api mybooking

--- src/components/booking/booking-list.tsx
+++ src/components/booking/booking-list.tsx
@@ -8,7 +8,7 @@ export default function BookingList({ bookings }: { bookings: IBooking[] }) {
     (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
   );
   return (
-    <div className="grid md:grid-cols-2  gap-3 xl:flex xl:flex-col xl:gap-5 mb-5">
+    <div className="grid md:grid-cols-2 gap-3 xl:flex xl:flex-col xl:gap-5 mb-5">
       {sortedBookings.map((booking) => (
         <BookingCard key={booking._id} booking={booking} />
       ))}
